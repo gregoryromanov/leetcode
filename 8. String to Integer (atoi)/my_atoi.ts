504cd66ee9374223f35b8af6d70cc9e5cf707500
gregoryromanov <gregoryromanov@gmail.com>
2021-06-04 12:34:50 +0300

    myAtoi in ts

---
+++ 8. String to Integer (atoi)/my_atoi.ts
@@ -0,0 +1,33 @@
+function myAtoi(s: string): number {
+  let result: string | number = '';
+  let nums: RegExp = /[0-9]/;
+  let allow_chars: RegExp = /[+-]/;
+
+  for (let i = 0; i < s.length; i++) {
+    if (result.length === 0 && s[i] === " ") continue;
+    else if (
+      result.length === 0 &&
+      s[i].search(allow_chars) === 0 &&
+      s[i + 1]?.search(nums) === 0
+    ) {
+      result += s[i];
+    } else if (s[i].search(nums) === 0) result += s[i];
+    else break;
+  }
+
+  result = Number(result);
+
+  if (result < Math.pow(-2, 31)) return Math.pow(-2, 31);
+  else if (result > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
+  return result;
+}
+
+console.log(myAtoi('21474836460'));
+console.log(myAtoi('+'));
+console.log(myAtoi('  +  42'));
+console.log(myAtoi('42'));
+console.log(myAtoi('   -42'));
+console.log(myAtoi('4193 with words'));
+console.log(myAtoi('words and 987'));
+console.log(myAtoi('-91283472332'));
+console.log(myAtoi('91283472332'));
